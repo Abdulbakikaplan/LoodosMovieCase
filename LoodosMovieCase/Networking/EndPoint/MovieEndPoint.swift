fc03a5c54d52bb42ea5e9c0649d45bfeeff56170
Abdülbaki Kaplan <abdulbakikaplan@hotmail.com>
2020-06-13 12:58:16 +0300

    Networking created

---
+++ LoodosMovieCase/Networking/EndPoint/MovieEndPoint.swift
@@ -0,0 +1,70 @@
+//
+//  MovieEndPoint.swift
+//  LoodosMovieCase
+//
+//  Created by Abdülbaki Kaplan on 13.06.2020.
+//  Copyright © 2020 Baki. All rights reserved.
+//
+
+import Foundation
+enum NetworkEnvironment {
+    case qa
+    case production
+    case staging
+}
+
+public enum MovieApi {
+    case movie(query: String, page:Int)
+    case movieDetail(id: String)
+}
+
+extension MovieApi: EndPointType {
+    
+    var environmentBaseURL : String {
+        switch NetworkManager.environment {
+            case .production: return "http://www.omdbapi.com/"
+        case .qa: return ""
+        case .staging: return ""
+        }
+    }
+    
+    var baseURL: URL {
+        guard let url = URL(string: environmentBaseURL) else { fatalError("baseURL could not be configured.")}
+        return url
+    }
+    
+    var path: String {
+        switch self {
+        case .movie(_, _), .movieDetail(_):
+            return ""
+        }
+    }
+    
+    var httpMethod: HTTPMethod {
+        return .get
+    }
+    
+    var task: HTTPTask {
+        switch self {
+            // case for send parameter(body)
+            case .movie(let query, let page):
+                return .requestParameters(bodyParameters: nil, bodyEncoding: .urlEncoding, urlParameters: [
+                    "s": query,
+                    "page": page,
+                    "type": "movie",
+                    "apikey":NetworkManager.MovieAPIKey
+                ])
+            case .movieDetail(let id):
+                return .requestParameters(bodyParameters: nil, bodyEncoding: .urlEncoding, urlParameters: [
+                    "i": id,
+                    "apikey":NetworkManager.MovieAPIKey
+                ])
+        default:
+            return .request
+        }
+    }
+    
+    var headers: HTTPHeaders? {
+        return nil
+    }
+}
---
+++ LoodosMovieCase/Networking/Service/HTTPMethod.swift
@@ -0,0 +1,17 @@
+//
+//  HTTPMethod.swift
+//  LoodosMovieCase
+//
+//  Created by Abdülbaki Kaplan on 13.06.2020.
+//  Copyright © 2020 Baki. All rights reserved.
+//
+
+import Foundation
+
+public enum HTTPMethod: String {
+    case get    = "GET"
+    case post   = "POST"
+    case put    = "PUT"
+    case patch  = "PATCH"
+    case delete = "DELETE"
+}
